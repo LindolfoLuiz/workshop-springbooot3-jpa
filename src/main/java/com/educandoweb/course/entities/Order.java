750eb875a228ad03385ce5ccfafbc9b7b975e9f2
LindolfoLuiz <lindolfofacu@hotmail.com>
2023-02-27 15:15:55 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,12 +2,16 @@ package com.educandoweb.course.entities;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "tb_order")
@@ -26,6 +30,8 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
     public Order() {
     }
 
@@ -70,6 +76,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -1,10 +1,12 @@
 package com.educandoweb.course.repositories;
 
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
